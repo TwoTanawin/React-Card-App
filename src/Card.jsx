2f1cf76f18680134f8bf7854a20308bc5ad4df41
twotanawin <tanawin.twox@gmail.com>
2024-06-09 17:52:52 +0700

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-img" src="https://image.spreadshirtmedia.com/image-server/v1/compositions/T210A22PA4301PT17X41Y40D1050125938W25000H23950/views/1,width=550,height=550,appearanceId=22,backgroundColor=750033,noPt=true/baby-dont-hurt-me-funny-meme-mens-t-shirt.jpg" alt="profile picture"></img>
+            <h2 className="card-title">Bro ISE</h2>
+            <p className="card-text">I am a Software Engineer</p>
+        </div>
+    );
+}
+
+export default Card
